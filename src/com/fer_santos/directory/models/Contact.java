bd32af548bb479f6259c2834ab86af45034c0ba2
Fernando Gordillo <gordillo.fernando10@hotmail.com>
2026-03-11 12:20:24 -0600

    feat: create User and Contact base models for the directory

--- src/com/fer_santos/directory/models/Contact.java
+++ src/com/fer_santos/directory/models/Contact.java
@@ -1,4 +1,70 @@
 package com.fer_santos.directory.models;
 
 public class Contact {
+  private String name;
+  private String lastName;
+  private String phoneNumber;
+  private String email;
+  private String alias;
+
+  public Contact(String name, String lastName, String phoneNumber, String email, String alias) {
+    this.name = name;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+    this.alias = alias;
+  }
+
+  public String getAlias() {
+    return alias;
+  }
+
+  // ### Getters & Setters
+  public void setAlias(String alias) {
+    this.alias = alias;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder("Contact{");
+    sb.append("name='").append(name).append('\'');
+    sb.append(", lastName='").append(lastName).append('\'');
+    sb.append(", phoneNumber='").append(phoneNumber).append('\'');
+    sb.append(", email='").append(email).append('\'');
+    sb.append(", alias='").append(alias).append('\'');
+    sb.append('}');
+    return sb.toString();
+  }
 }
--- src/com/fer_santos/directory/models/User.java
+++ src/com/fer_santos/directory/models/User.java
@@ -1,4 +1,64 @@
 package com.fer_santos.directory.models;
 
+import java.util.ArrayList;
+
 public class User {
+  private ArrayList<Contact> contacts = new ArrayList<>();
+
+  private String name;
+  private String lastName;
+  private String email;
+  private String password;
+
+  public User(String password, String email, String lastName, String name) {
+    this.password = password;
+    this.email = email;
+    this.lastName = lastName;
+    this.name = name;
+  }
+
+  public void addContact() {
+
+  }
+
+  public void deleteContact() {
+
+  }
+
+  public void modifyContact() {
+
+  }
+
+  // ### Getters & Setters
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
 }
